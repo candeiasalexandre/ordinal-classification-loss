e2aa8b35959c55964316af714d91bf39be8f8279
candeiasalexandre <candeiasalexandre@gmail.com>
2023-05-19 22:13:22 +0100

    added diff_ordinal_cross_entropy, changed tests to parametrize and added new loss

--- ordinal_classification_loss/loss.py
+++ ordinal_classification_loss/loss.py
@@ -1,6 +1,7 @@
 from typing import Optional
 import torch
 
+
 def non_diff_ordinal_cross_entropy(
     input: torch.Tensor,
     target: torch.Tensor,
@@ -24,3 +25,39 @@ def non_diff_ordinal_cross_entropy(
         return loss.mean()
 
     raise ValueError("reduction should be None, sum or mean!")
+
+
+def _create_weight_matrix(num_classes: int) -> torch.Tensor:
+    W_matrix = []
+    for i in range(num_classes):
+        W_matrix.append([abs(i - j) for j in range(num_classes)])
+
+    return torch.tensor(W_matrix)
+
+
+def diff_ordinal_cross_entropy(
+    input: torch.Tensor,
+    target: torch.Tensor,
+    alpha: float = 1,
+    reduction: Optional[str] = "mean",
+) -> torch.Tensor:
+
+    num_classes = input.shape[1]
+    weights_matrix = _create_weight_matrix(num_classes)
+
+    ce = torch.nn.functional.cross_entropy(input, target, reduction="none")
+
+    prob = torch.nn.functional.softmax(input, dim=1)
+    reg = weights_matrix[target, :] * prob
+    reg = reg.sum(dim=1)
+
+    loss = ce + alpha * reg
+
+    if reduction is None or reduction == "none":
+        return loss
+
+    if reduction == "sum":
+        return loss.sum()
+
+    if reduction == "mean":
+        return loss.mean()
--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,5 +1,9 @@
 import pytest
-from ordinal_classification_loss.loss import non_diff_ordinal_cross_entropy
+from ordinal_classification_loss.loss import (
+    non_diff_ordinal_cross_entropy,
+    _create_weight_matrix,
+    diff_ordinal_cross_entropy,
+)
 from torch.nn.functional import cross_entropy
 import torch
 
@@ -20,46 +24,77 @@ def labels() -> torch.Tensor:
     return torch.tensor([0, 0, 0])
 
 
+
+def assert_is_not_close(x: torch.Tensor, y: torch.Tensor) -> None:
+    assert not torch.all(torch.isclose(x, y))
+
 def test_ce_loss(logits: torch.Tensor, labels: torch.Tensor) -> None:
     loss = cross_entropy(logits, labels, reduce=False)
     print(f"cross_entropy loss: \n {loss}")
     assert loss is not None
 
 
-class TestNonDiffOrdinalCrossEntropy:
-    def test_alpha_0(self, logits: torch.Tensor, labels: torch.Tensor) -> None:
+class TestOrdinalCrossEntropy:
+    @pytest.mark.parametrize(
+        "loss_fn", [non_diff_ordinal_cross_entropy, diff_ordinal_cross_entropy]
+    )
+    def test_alpha_0(self, loss_fn, logits: torch.Tensor, labels: torch.Tensor) -> None:
         loss_ce = cross_entropy(logits, labels, reduce=False)
-        loss_ordinal_ce = non_diff_ordinal_cross_entropy(
-            logits, labels, reduction="none", alpha=0
-        )
+        loss_ordinal_ce = loss_fn(logits, labels, reduction="none", alpha=0)
 
         torch.testing.assert_close(loss_ce, loss_ordinal_ce)
 
-    def test_values(self, logits: torch.Tensor, labels: torch.Tensor) -> None:
+    @pytest.mark.parametrize(
+        "loss_fn, expected_loss",
+        [
+            (
+                non_diff_ordinal_cross_entropy,
+                torch.tensor([1.8194, 1.4194, 1.8194]) + torch.tensor([4, 0, 1]),
+            ),
+            (
+                diff_ordinal_cross_entropy,
+                # ce + reg calculated for W0 * softmax(logits)
+                torch.tensor([1.8194, 1.4194, 1.8194]) + torch.tensor([2.1991, 1.8009, 1.9903]),
+            ),
+        ],
+    )
+    def test_values(
+        self, loss_fn, expected_loss, logits: torch.Tensor, labels: torch.Tensor
+    ) -> None:
 
-        loss = non_diff_ordinal_cross_entropy(logits, labels, reduction="none")
+        loss = loss_fn(logits, labels, reduction="none")
 
-        print(f"non_diff_ordinal_cross_entropy loss: \n {loss}")
+        print(f"{loss_fn.__name__} loss: \n {loss}")
+        torch.testing.assert_close(loss, expected_loss, atol=1E-4, rtol=1E-4)
 
-        expected_loss = cross_entropy(logits, labels, reduce=False) + torch.tensor(
-            [4, 0, 1]
-        )
-        torch.testing.assert_close(loss, expected_loss)
-
-    def test_gradient(self, logits: torch.Tensor, labels: torch.Tensor) -> None:
+    @pytest.mark.parametrize(
+        "loss_fn, assert_fn",
+        [
+            (non_diff_ordinal_cross_entropy, torch.testing.assert_close),
+            (diff_ordinal_cross_entropy, assert_is_not_close),
+        ],
+    )
+    def test_gradient(
+        self, loss_fn, assert_fn, logits: torch.Tensor, labels: torch.Tensor
+    ) -> None:
         logits_ce = torch.tensor(logits.numpy(), requires_grad=True)
         loss_ce = cross_entropy(logits_ce, labels, reduction="mean")
         loss_ce.backward()
         grad_ce = logits_ce.grad
 
         logits_oce = torch.tensor(logits.numpy(), requires_grad=True)
-        loss_ordinal_ce = non_diff_ordinal_cross_entropy(
-            logits_oce, labels, reduction="mean", alpha=1
-        )
+        loss_ordinal_ce = loss_fn(logits_oce, labels, reduction="mean", alpha=1)
         loss_ordinal_ce.backward()
         grad_oce = logits_oce.grad
 
         print(f"Gradient cross entropy: \n {grad_ce}")
-        print(f"Gradient non differential ordinal cross entropy: \n {grad_oce}")
+        print(f"Gradient {loss_fn.__name__}: \n {grad_oce}")
 
-        torch.testing.assert_close(grad_ce, grad_oce)
+        assert_fn(grad_ce, grad_oce)
+
+
+def test_create_weight_matrix() -> None:
+    num_classes = 3
+    expected_matrix = torch.tensor([[0, 1, 2], [1, 0, 1], [2, 1, 0]])
+    matrix = _create_weight_matrix(num_classes)
+    torch.testing.assert_close(matrix, expected_matrix)
